a9c02600bac77573b8d12bf1f92414b2980733b7
Anas <anasmuhammed444@gmail.com>
2024-01-09 17:04:40 +0530

    Building game cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Grid, GridItem,Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+
 function App() {
   return (
     <Grid
@@ -17,7 +19,7 @@ function App() {
       </Show>
      
       <GridItem area="main">  
-        Main
+        <GameGrid />
       </GridItem>
     </Grid>
   );
